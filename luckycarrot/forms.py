2ebea300f5774930c73625b230d9a8eab025aa62
Hassan <hassan.rachid5@gmail.com>
2020-08-14 03:24:15 -0400

    added login features

--- luckycarrot/forms.py
+++ luckycarrot/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import SubmitField, StringField, PasswordField, BooleanField
-from wtforms.validators import DataRequired, Email, Length, EqualTo
+from wtforms.validators import DataRequired, Email, Length, EqualTo, ValidationError
+from luckycarrot.models import User
 
 
 class RegistrationForm(FlaskForm):
@@ -10,7 +11,18 @@ class RegistrationForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired()])
     confirm_password = PasswordField('Confirm Password', validators=[
                                      DataRequired(), EqualTo('password')])
+    updates = BooleanField('I would like to receive email updates.')
     submit = SubmitField('Sign Up')
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError('Username taken. Please try again.')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError('Email taken. Please try again.')
 
 
 class LoginForm(FlaskForm):
--- luckycarrot/routes.py
+++ luckycarrot/routes.py
@@ -1,7 +1,8 @@
 from flask import render_template, url_for, request, redirect, flash
-from luckycarrot import app
+from luckycarrot import app, db, bcrypt
 from luckycarrot.forms import RegistrationForm, LoginForm
 from luckycarrot.models import User, Post
+from flask_login import login_user, current_user, logout_user
 
 
 @app.route("/", methods=['GET'])
@@ -27,11 +28,18 @@ def updates():
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+
     form = RegistrationForm()
 
     if form.validate_on_submit():
-        flash(f'Account created for {form.username.data}!', 'success')
-        return redirect(url_for('register'))
+        hashed_pw = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        user = User(username=form.username.data, email=form.email.data, password=hashed_pw, updates=form.updates.data)
+        db.session.add(user)
+        db.session.commit()
+        flash(f'Account created! You can now log in.', 'success')
+        return redirect(url_for('login'))
 
     return render_template('register.html', form=form)
 
@@ -39,12 +47,24 @@ def register():
 @app.route('/login', methods=["GET", "POST"])
 def login():
     form = LoginForm()
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
 
     if form.validate_on_submit():
-        if form.email.data == 'test@gmail.com' and form.password.data == '123':
-            flash(f'You have been logged in!', 'success')
-            return redirect(url_for('login'))
+        
+        user = User.query.filter_by(email=form.email.data).first()
+        
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            login_user(user, remember=form.remember.data)
+            return redirect(url_for('home'))
+
         else:
             flash(f'Login Unsuccessful. Please try again.', 'danger')
 
     return render_template('login.html', form=form)
+
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
