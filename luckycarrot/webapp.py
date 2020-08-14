bc22029ad898a8e5569aad66229b9ce1ff6a6a83
Hassan <hassan.rachid5@gmail.com>
2020-08-14 00:35:40 -0400

    added about page

--- luckycarrot/webapp.py
+++ luckycarrot/webapp.py
@@ -6,7 +6,22 @@ app = Flask(__name__)
 @app.route("/")
 @app.route("/home")
 def home():
-    return render_template("home.html")
+    return render_template("home.html", home=True)
+
+
+@app.route("/about")
+def about():
+    return render_template('about.html', home=False)
+
+
+@app.route("/blog")
+def blog():
+    return render_template('blog.html', home=False)
+
+
+@app.route('/updates')
+def updates():
+    return render_template('updates.html', home=False)
 
 
 if __name__ == '__main__':
