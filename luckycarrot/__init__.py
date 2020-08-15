3c296ec8e5113ee2cc89685436034000c0619dcd
Hassan <hassan.rachid5@gmail.com>
2020-08-15 01:07:49 -0400

    added sidebar to error pages

--- luckycarrot/__init__.py
+++ luckycarrot/__init__.py
@@ -28,9 +28,11 @@ def create_app(config_class=Config):
     from luckycarrot.users.routes import users
     from luckycarrot.posts.routes import posts
     from luckycarrot.main.routes import main
+    from luckycarrot.errors.handlers import errors
 
     app.register_blueprint(users)
     app.register_blueprint(posts)
     app.register_blueprint(main)
+    app.register_blueprint(errors)
 
     return app
