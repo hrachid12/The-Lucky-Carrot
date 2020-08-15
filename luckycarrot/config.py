98c52504a581bae5b5f7d318f7c13f9c1520c4d6
Hassan <hassan.rachid5@gmail.com>
2020-08-14 20:53:15 -0400

    removed updates page

--- luckycarrot/config.py
+++ luckycarrot/config.py
@@ -1,10 +1,10 @@
 import os
 
 class Config:
-    SECRET_KEY = '6d229a0fceeaf708f0737a0aa9cfb2b8'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
+    SECRET_KEY = os.environ.get('LUCKY_SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('LUCKY_DB')
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
-    MAIL_USERNAME = 'theluckycarrot1@gmail.com'
-    MAIL_PASSWORD = 'HRachid12!gmail'
+    MAIL_USERNAME = os.environ.get('LUCKY_EMAIL_USER')
+    MAIL_PASSWORD = os.environ.get('LUCKY_EMAIL_PASS')
